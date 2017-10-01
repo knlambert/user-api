05f83b3e9a3beebebdea15a40e3cc86b00a3fa4e
Kévin LAMBERT <kevin.lambert@free.fr>
2017-10-01 15:52:14 +0200

    Add redirection login URL

--- user_api/flask_user_api.py
+++ user_api/flask_user_api.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from functools import wraps
-from flask import request, jsonify, Blueprint
+from flask import request, jsonify, Blueprint, redirect
 import base64
 import re
 import json
@@ -12,35 +12,42 @@ class FlaskUserApi(object):
     def __init__(self, user_api):
         self.user_api = user_api
 
-    def is_connected(self, funct):
-        @wraps(funct)
-        def wrapper(*args, **kwargs):
-            # These two endpoint do not hae self.user_api.authentication.
-            if request.endpoint not in [u'user_api.authentify']:
+    def is_connected(self, login_url=None):
 
-                if u"Authorization" in request.headers:
-                    authorization = request.headers.get(u"Authorization")
-                    m = re.search(u"Bearer (\S+)", authorization)
-                    token = m.group(1)
-                    if m is None or not self.user_api.authentication.is_token_valid(token):
-                        return jsonify({
-                            u"Message": u"Invalid token."
-                        }), 401
-                elif u"user-api-credentials" in request.cookies:
-                    decoded_token = base64.b64decode(request.cookies.get(u'user-api-credentials'))
-                    if not self.user_api.authentication.is_token_valid(decoded_token):
-                        return jsonify({
-                            u"Message": u"Invalid token."
-                        }), 401
-                else:
-                    return jsonify({
-                        u"Message": u"Unauthorized."
-                    }), 403
-                # If all right, do call function
-            ret = funct(*args, **kwargs)
-            return ret
+        def decorator(funct):
+            @wraps(funct)
+            def wrapper(*args, **kwargs):
+                # These two endpoint do not hae self.user_api.authentication.
+                if request.endpoint not in [u'user_api.authentify']:
 
-        return wrapper
+                    if u"Authorization" in request.headers:
+                        authorization = request.headers.get(u"Authorization")
+                        m = re.search(u"Bearer (\S+)", authorization)
+                        token = m.group(1)
+                        if m is None or not self.user_api.authentication.is_token_valid(token):
+                            return jsonify({
+                                u"message": u"Invalid token."
+                            }), 401
+                    elif u"user-api-credentials" in request.cookies:
+                        decoded_token = base64.b64decode(request.cookies.get(u'user-api-credentials'))
+                        if not self.user_api.authentication.is_token_valid(decoded_token):
+                            return jsonify({
+                                u"message": u"Invalid token."
+                            }), 401
+                    else:
+                        if login_url:
+                            return redirect(login_url, 302)
+                        else:
+                            return jsonify({
+                                u"message": u"Forbidden."
+                            }, 403)
+                    # If all right, do call function
+                ret = funct(*args, **kwargs)
+                return ret
+
+            return wrapper
+
+        return decorator
 
     def construct_blueprint(self):
         user_api_blueprint = Blueprint(u'user_api', __name__)
@@ -93,7 +100,7 @@ class FlaskUserApi(object):
                 }), 422
 
         @user_api_blueprint.route(u'/reset-password', methods=[u'POST'])
-        @self.is_connected
+        @self.is_connected()
         def reset_password():
             try:
                 data = json.loads(request.data, encoding=u"utf-8")
@@ -124,7 +131,7 @@ class FlaskUserApi(object):
                 }), 422
 
         @user_api_blueprint.route(u'/register', methods=[u"POST"])
-        @self.is_connected
+        @self.is_connected()
         def register():
             try:
                 data = json.loads(request.data, encoding=u"utf-8")
@@ -155,14 +162,14 @@ class FlaskUserApi(object):
                 }),
 
         @user_api_blueprint.route(u'/me', methods=[u"GET"])
-        @self.is_connected
+        @self.is_connected()
         def me():
             if u"user-api-credentials" in request.cookies:
                 decoded_token = base64.b64decode(request.cookies.get(u'user-api-credentials'))
                 return jsonify(self.user_api.authentication.get_token_data(decoded_token)), 200
 
         @user_api_blueprint.route(u'/logout', methods=[u"GET"])
-        @self.is_connected
+        @self.is_connected()
         def logout():
             if u"user-api-credentials" in request.cookies:
                 response = jsonify({
