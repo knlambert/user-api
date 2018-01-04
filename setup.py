aa026e6fc9d06f5f462ffab53234830c23ff7fbc
Kevin LAMBERT <kevin.lambert@free.fr>
2018-01-03 23:07:17 -0500

    correct setup

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ from setuptools import setup
 setup(
     name=u'user_api',    # This is the name of your PyPI-package.
     version=u'0.3',                          # Update the version number for new releases
-    packages=[u'user_api'],
+    packages=[
+        u'user_api',
+        u"user_api.adapter"
+    ],
     install_requires=[
         u"ecdsa",
         u'flask',
