f895ed40033e8a58d14782ec323fa70ca5b195a1
Kevin LAMBERT <kevin.lambert@free.fr>
2018-02-24 22:19:04 -0500

    Add exception imports.

--- user_api/__init__.py
+++ user_api/__init__.py
@@ -1,2 +1,10 @@
 from .user_api import UserApi
-from .user_api_exception import *
+from .user_api_exception import (
+    ApiException,
+    ApiConflict,
+    ApiForbidden,
+    ApiNotFound,
+    ApiRoleMissing,
+    ApiUnauthorized,
+    ApiUnprocessableEntity
+)
