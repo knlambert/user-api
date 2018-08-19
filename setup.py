607c9c50ef028776a6049fa51a348ba7d0635c08
Kevin LAMBERT <kevin.lambert@free.fr>
2018-08-19 15:06:10 -0400

    Correct typo in setup.py file.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup
 
 setup(
-    name="user_api',    # This is the name of your PyPI-package.
-    version="0.5.0',                          # Update the version number for new releases
+    name="user_api", 
+    version="0.5.0",   
     packages=[
-        "user_api',
+        "user_api",
         "user_api.adapter",
         "user_api.adapter.flask",
         "user_api.auth",
@@ -13,9 +13,9 @@ setup(
     ],
     install_requires=[
         "ecdsa==0.13",
-        "flask>=1.0.2,<2',
+        "flask>=1.0.2,<2",
         "PyJWT>=1.6.4,<2",
-        "SQLAlchemy>=1.2,<2',
+        "SQLAlchemy>=1.2,<2",
         "Cerberus>=1.2,<2",
         "pycryptodome>=3.6,<4",
         "mysql-connector>=2.1.6,<3"
